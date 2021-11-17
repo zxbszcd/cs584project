cee5118a52fd39f08e1dc359fb34a40ae3190ffc
FengHe <670096404@qq.com>
2021-11-17 22:57:04 +0800

    add main function

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from option import args
 from checkpoint import Checkpoint
 from trainer import Trainer
 
-utility.set_seed(args.seed)
-checkpoint = Checkpoint(args)
+if __name__ == '__main__':
+    utility.set_seed(args.seed)
+    checkpoint = Checkpoint(args)
 
-if checkpoint.ok:
-    loader = data.Data(args)
-    model = model.Model(args, checkpoint)
-    loss = loss.Loss(args, checkpoint) if not args.test_only else None
-    t = Trainer(args, loader, model, loss, checkpoint)
-    while not t.terminate():
-        t.train()
-        t.test()
-    checkpoint.done()
+    if checkpoint.ok:
+        loader = data.Data(args)
+        model = model.Model(args, checkpoint)
+        loss = loss.Loss(args, checkpoint) if not args.test_only else None
+        t = Trainer(args, loader, model, loss, checkpoint)
+        while not t.terminate():
+            t.train()
+            t.test()
+        checkpoint.done()
 
